f2ca815251ff51009781e4fd776e182708e8a35b
10 <10@DESKTOP-U0681NO>
2025-09-21 23:36:09 +0300

    Create

---
+++ Company.G01.PL/DTOs/CreateDepartmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G01.PL.DTOs
+{
+    public class CreateDepartmentDTO
+    {
+        [Required(ErrorMessage = "Code is Required !")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is Required !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "CreateAt is Required !")]
+        public DateTime CreateAt { get; set; }
+
+    }
+}
