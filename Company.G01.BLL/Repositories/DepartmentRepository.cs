cd98c273cb870d2b7843a98be63da11ccecfb3b8
10 <10@DESKTOP-U0681NO>
2025-09-21 17:54:37 +0300

    Department Module BLL

---
+++ Company.G01.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Company.G01.BLL.Interfaces;
+using Company.G01.DAL.Data.Contexts;
+using Company.G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G01.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+
+        private readonly CompanyDbContext _context;
+
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+
+
+        public IEnumerable<Department> GetAll()
+        {
+          
+           return _context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+           
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+           
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+            
+            _context.Departments.Update(model);
+            return _context.SaveChanges(); 
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+    }
+}
